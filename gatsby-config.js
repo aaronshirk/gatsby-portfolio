f2c1231796bab7b480c52ada6dd446a83440351c
Aaron Shirk <aaronshirk@gmail.com>
2023-08-12 20:54:33 -0400

    scaffold initial styling and card section

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,29 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `Caveat`,
+            file: `https://fonts.googleapis.com/css2?family=Caveat&display=swap`,
+          },
+          {
+            name: `Montserrat`,
+            file: `https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap`,
+          },
+        ],
+      },
+    },
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,113 @@
-import * as React from "react"
+import * as React from "react";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { StaticImage } from "gatsby-plugin-image";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <main className="bg-white px-10 md:px-20 lg:px-40">
+      <section className="mb-10">
+        <nav className="py-10 mb-12 flex justify-between">
+          <h1 className="text-xl font-serif">developedbyaaron</h1>
+          <ul className="flex items-center">
+            <li>
+              <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+            </li>
+            <li>
+              <a
+                className="bg-teal-500 text-white px-4 py-2 rounded-lg ml-4 md:ml-6 lg:ml-8"
+                href="#"
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="text-center p-5">
+          <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
+            Aaron Shirk
+          </h2>
+          <h3 className="text-2xl md:text-3xl">
+            Developer + Data Scientist + Drummer
+          </h3>
+          <p className="text-md py-5 leading-8 text-gray-700 md:text-lg max-w-xl mx-auto">
+            Experienced software developer, engineer, and budding data
+            scientist. Check out some of my past work and projects and connect
+            with me down below.
+          </p>
+        </div>
+        <div className="text-5xl flex justify-center gap-16 text-stone-600">
+          <AiFillLinkedin />
+          <AiFillGithub />
+        </div>
+        <StaticImage
+          src="../images/Profile_Pic.jpg"
+          layout="fixed"
+          width={200}
+          height={200}
+          className="relative rounded-full mx-auto mt-6"
+        />
+      </section>
+
+      {/* ***  PROJECTS / PORTFOLIO *** */}
+      <section>
+        <div className="text-left md:text-center">
+          <h3 className="text-3xl pt-2">Featured Projects</h3>
+          <p className="text-md py-3 text-gray-700">
+            View selected projects below. More information can be found at{" "}
+            <a href="https://aaronshirk.github.io">aaronshirk.github.io</a>.
+          </p>
+
+          {/* Project 1 */}
+          <div>
+            <div className="bg-zinc-50 text-center shadow-xl p-10 rounded-xl my-10 max-w-sm">
+              <StaticImage
+                src="../images/andoid-apps-ratings.jpg"
+                alt="Android app rating distribution"
+                className="w-5/6"
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                The Android App Market on Google Play (Jupyter Notebook)
+              </h3>
+              <p className="text-md text-gray-700 mb-5">
+                Comprehensive analysis of the Android app market, comparing over
+                ten thousand apps in Google Play across different categories
+              </p>
+              <a
+                className="bg-teal-500 text-white px-4 py-2 rounded-lg hover:bg-teal-600 hover:pointer"
+                href="https://github.com/aaronshirk/android-app-market/blob/main/notebook.ipynb"
+                target="_blank"
+              >
+                View
+              </a>
+            </div>
+
+            {/* Project 2 */}
+            <div className="bg-zinc-50 text-center shadow-xl p-10 rounded-xl my-10 max-w-sm">
+              <StaticImage
+                src="../images/netflix-movies.jpg"
+                alt="Investigating netflix movie length"
+                className="w-5/6"
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Investigating Netflix Movies and Guest Stars in The Office
+              </h3>
+              <p className="text-md text-gray-700 mb-5">
+                From Netflix movies data, can we determine that the average
+                movie duration has been decreasing steadily over the last
+                decade?
+              </p>
+              <a
+                className="bg-teal-500 text-white px-4 py-2 rounded-lg hover:bg-teal-600"
+                href="https://github.com/aaronshirk/android-app-market/blob/main/notebook.ipynb"
+                target="_blank"
+              >
+                View
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
