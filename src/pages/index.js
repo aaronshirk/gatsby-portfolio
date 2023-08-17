0d1fd1fbb6b53e8a982f88bf7d55a9102fec69cc
Aaron Shirk <aaronshirk@gmail.com>
2023-08-16 21:13:38 -0400

    add content and style first job

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 export default function Home() {
   return (
     <main className="bg-white px-10 md:px-20 lg:px-40">
+      {/* *** ABOUT / PROFILE *** */}
       <section className="mb-10">
         <nav className="py-10 mb-12 flex justify-between">
           <h1 className="text-xl font-serif">developedbyaaron</h1>
@@ -52,9 +53,9 @@ export default function Home() {
       {/* ***  PROJECTS / PORTFOLIO *** */}
       <section>
         <div>
-          <h3 className="text-3xl pt-2 text-left md:text-center">
+          <h2 className="text-3xl pt-2 text-left md:text-center">
             Featured Projects
-          </h3>
+          </h2>
           <p className="text-md py-3 text-gray-700 text-left md:text-center">
             View selected projects below. More information can be found at{" "}
             <a href="https://aaronshirk.github.io">aaronshirk.github.io</a>.
@@ -109,6 +110,171 @@ export default function Home() {
               </a>
             </div>
           </div>
+        </div>
+      </section>
+
+      {/* ***  WORK EXPERIENCE  *** */}
+      <section>
+        <div>
+          <h2 className="text-xl pt-2 text-left md:text-center">
+            Work Experience
+          </h2>
+          <p className="text-md py-3 text-gray-700 text-left md:text-center">
+            See my complete work history on{" "}
+            <a
+              href="https://www.linkedin.com/in/aaronshirk/"
+              className="text-blue-600 visited:text-purple-600"
+            >
+              LinkedIn
+            </a>
+            .
+          </p>
+
+          {/* Job 1 */}
+          <section>
+            <div className="mb-5">
+              <h3 className="text-lg leading-6 font-semibold mt-6">
+                Senior Analytics Engineer
+              </h3>
+              <p className="italic leading-6 text-md text-gray-700">
+                Premier Inc.
+              </p>
+              <p className="text-md leading-6 text-gray-700">
+                Apr 22 - Present
+              </p>
+            </div>
+            <div className="mb-5">
+              <p className="text-md text-gray-700 mb-5">
+                Build data analytics reports, data pipelines
+              </p>
+              <p className="text-md text-gray-700 mb-2">Skills:</p>
+              <ul className="list-disc list-inside">
+                <li>React.js</li>
+                <li>Typescript</li>
+                <li>PostgreSQL</li>
+                <li>Apache Airflow</li>
+                <li>Software Development</li>
+                <li>SQL</li>
+                <li>Python</li>
+                <li>Pandas</li>
+              </ul>
+            </div>
+          </section>
+
+          {/* Job 2 */}
+          <section>
+            <div>
+              <h3>Senior Software Developer</h3>
+              <p>North Wind Group</p>
+              <p>Oct 21 - Apr 22</p>
+            </div>
+            <div>
+              <p>
+                Served as lead software developer to help establish modern
+                software development practices
+              </p>
+              <ul>
+                <li>
+                  Lead the development team towards modern application
+                  development using ReactJS, ASP.NET Core, Sql Server as the
+                  primary development stack and Azure and Azure DevOps for
+                  managed cloud services
+                </li>
+                <li>
+                  Develop and maintain corporate applications utilized by all
+                  subsidiary businesses; example applications include Vaccine
+                  Status System, Lease Application, Total Compensation, and
+                  Purchase Requisition
+                </li>
+                <li>
+                  Establish, promote, and train on standard development
+                  practices, process, and coding standards based on Agile and
+                  modern development workflows (i.e. git/github
+                </li>
+                <li>GitFlow, dev build/deploy pipelines)</li>
+                <li>
+                  Maintain corporate legacy applications in Drupal/Ubuntu, MS
+                  SharePoint, ASP.NET MVC
+                </li>
+              </ul>
+            </div>
+          </section>
+
+          {/* Job 3 */}
+          <section>
+            <div>
+              <h3>Full Stack Developer (Contract and Full-Time)</h3>
+              <p>PWC</p>
+              <p>Aug 20 - Sep 21</p>
+            </div>
+            <div>
+              <ul>
+                <li>
+                  Develop and build out two APIs for the Automated Contact
+                  Tracing (ACT) application using ASP.NET Core, C#,
+                  EntityFramework Core, and Sql Server
+                </li>
+                <li>
+                  Implement new data import features using Azure Functions (C#)
+                  primarily used for bulk-importing of large sets of contact
+                  tracing user and device data into the ACT application database
+                </li>
+                <li>
+                  Build new API and Progressive Web App (PWA) UI for processing
+                  user checkin status; Technologies used include Angular,
+                  ASP.NET Core, EF Core, Sql Server
+                </li>
+              </ul>
+            </div>
+          </section>
+
+          {/* Job 4 */}
+          <section>
+            <div>
+              <h3>Software Developer</h3>
+              <p>JMT Technology Group</p>
+              <p>Sep 19 - Aug 20</p>
+            </div>
+            <div>
+              <ul>
+                <li>
+                  Implement new features for JMT’s core and extensible outdoor
+                  licensing and registration platform with Angular 7,
+                  Typescript, .Net Framework, C# and SQL Server for the DB
+                  backend
+                </li>
+                <li>
+                  Build out multiple State licensing and registration websites
+                  by extending the core platform with State specific
+                  requirements, features and enhancements
+                </li>
+                <li>
+                  Develop and promote high-quality, scalable and secure State
+                  websites with modern languages, web frameworks, and tooling
+                  within fast-paced Agile teams under tight production deadlines
+                </li>
+              </ul>
+            </div>
+          </section>
+        </div>
+      </section>
+
+      {/* ***  EDUCATION & CERTIFICATIONS *** */}
+      <section>
+        <div>
+          <h2>Education</h2>
+
+          <section>
+            <h3>University of Colorado</h3>
+            <p>MS - Space Operations Engineering 1998</p>
+            <p>GPA: 4.00</p>
+          </section>
+
+          <section>
+            <h3>The Ohio State University</h3>
+            <p>BS - Computer Information Science and Engineering 1998</p>
+            <p>GPA: 3.89</p>
+          </section>
         </div>
       </section>
     </main>
