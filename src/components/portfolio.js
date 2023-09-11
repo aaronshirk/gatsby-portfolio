40e91fd4c4537ec6473c922c39bc322f8c238b5b
Aaron Shirk <aaronshirk@gmail.com>
2023-09-10 20:43:28 -0400

    finish gatsbyimage setup

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -17,10 +17,7 @@ export default function Portfolio() {
             title
             image {
               childImageSharp {
-                gatsbyImageData
-              }
-              childrenImageSharp {
-                gatsbyImageData
+                gatsbyImageData(height: 200, aspectRatio: 2.44)
               }
             }
           }
@@ -29,8 +26,6 @@ export default function Portfolio() {
       }
     }
   `);
-
-  console.log(nodes);
 
   return (
     <section>
@@ -46,58 +41,9 @@ export default function Portfolio() {
         <div className="md:flex gap-10">
           {nodes &&
             nodes.length > 0 &&
-            nodes.map((node, index) => (
+            nodes.map((node) => (
               <Project key={node.id} projectData={node.frontmatter} />
             ))}
-          {/* Project 1 */}
-          <div className="text-center shadow-xl rounded-xl my-10 w-96 h-96 dark:bg-slate-700 overflow-hidden">
-            {/* <StaticImage
-              src="../images/andoid-apps-ratings.jpg"
-              alt="Android app rating distribution"
-              height={200}
-              aspectRatio={2.44}
-            /> */}
-            <h3 className="text-lg font-medium mt-6 mb-3 mx-2 md:mx-4 dark:text-white">
-              The Android App Market on Google Play (Jupyter Notebook)
-            </h3>
-            <p className="text-md text-gray-700 mb-6 mx-2 md:mx-4 dark:text-gray-400">
-              Comprehensive analysis of the Android app market; Source: Google
-              Play
-            </p>
-            <a
-              className="bg-teal-500 text-white px-4 py-2 rounded-lg hover:bg-teal-600 hover:pointer"
-              href="https://github.com/aaronshirk/android-app-market/blob/main/notebook.ipynb"
-              target="_blank"
-              rel="noreferrer"
-            >
-              View
-            </a>
-          </div>
-
-          {/* Project 2 */}
-          <div className="text-center shadow-xl rounded-xl my-10 w-96 h-96 dark:bg-slate-700 overflow-hidden">
-            {/* <StaticImage
-              src="../images/netflix-movies.jpg"
-              alt="Investigating netflix movie length"
-              height={200}
-              aspectRatio={2.44}
-            /> */}
-            <h3 className="text-lg font-medium mt-6 mb-3 mx-2 md:mx-4 dark:text-white">
-              Investigating Netflix Movies and Guest Stars in The Office
-            </h3>
-            <p className="text-md text-gray-700 mb-6 mx-2 md:mx-4 dark:text-gray-400">
-              Is it true that movie duration has steadily decreased over the
-              last few years?
-            </p>
-            <a
-              className="bg-teal-500 text-white px-4 py-2 rounded-lg hover:bg-teal-600"
-              href="https://github.com/aaronshirk/android-app-market/blob/main/notebook.ipynb"
-              target="_blank"
-              rel="noreferrer"
-            >
-              View
-            </a>
-          </div>
         </div>
       </div>
     </section>
--- src/components/project.js
+++ src/components/project.js
@@ -1,30 +1,26 @@
 import React from "react";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 export default function Project(props) {
   const { projectData } = props;
-  console.log(projectData);
+  const image = getImage(projectData.image);
 
   return (
     <div className="text-center shadow-xl rounded-xl my-10 w-96 h-96 dark:bg-slate-700 overflow-hidden">
-      {/* <StaticImage
-              src="../images/andoid-apps-ratings.jpg"
-              alt="Android app rating distribution"
-              height={200}
-              aspectRatio={2.44}
-            /> */}
+      <GatsbyImage image={image} alt={projectData.image_alt} />
       <h3 className="text-lg font-medium mt-6 mb-3 mx-2 md:mx-4 dark:text-white">
         {projectData.title}
       </h3>
       <p className="text-md text-gray-700 mb-6 mx-2 md:mx-4 dark:text-gray-400">
-        Comprehensive analysis of the Android app market; Source: Google Play
+        {projectData.subtitle}
       </p>
       <a
         className="bg-teal-500 text-white px-4 py-2 rounded-lg hover:bg-teal-600 hover:pointer"
-        href="https://github.com/aaronshirk/android-app-market/blob/main/notebook.ipynb"
+        href={projectData.github_repo_url}
         target="_blank"
         rel="noreferrer"
       >
-        View
+        {projectData.cta}
       </a>
     </div>
   );
